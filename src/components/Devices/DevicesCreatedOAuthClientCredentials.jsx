392c09e2b667173bb0b799fa5d2c1b436843925e
Killian Courvoisier <killian.courvoisier@cozycloud.cc>
2026-01-26 17:19:45 +0000

    refactor: Use CozyUI Input component to handle client_id and client_secret after creation

--- src/components/Devices/DevicesCreatedOAuthClientCredentials.jsx
+++ src/components/Devices/DevicesCreatedOAuthClientCredentials.jsx
@@ -1,69 +1,34 @@
 import React from 'react'
 
-import Icon from 'cozy-ui/transpiled/react/Icon'
-import IconButton from 'cozy-ui/transpiled/react/IconButton'
-import CheckIcon from 'cozy-ui/transpiled/react/Icons/Check'
-import CopyIcon from 'cozy-ui/transpiled/react/Icons/Copy'
-import TextField from 'cozy-ui/transpiled/react/TextField'
 import Typography from 'cozy-ui/transpiled/react/Typography'
 
-const DevicesCreatedOAuthClientCredentials = ({
-  t,
-  created,
-  copiedField,
-  onCopy
-}) => {
+import Input from '@/components/Input'
+
+const DevicesCreatedOAuthClientCredentials = ({ t, created }) => {
   return (
     <>
       <Typography className="u-mb-1">
         {t('createOAuthClient.success_description')}
       </Typography>
 
-      <div className="u-flex u-flex-items-center u-mb-1">
-        <div className="u-flex-grow-1">
-          <TextField
-            label={t('createOAuthClient.client_id')}
-            value={created.clientId}
-            readOnly
-            fullWidth
-          />
-        </div>
-        <IconButton
-          className={
-            'u-ml-half u-mt-1' +
-            (copiedField === 'clientId' ? ' u-bg-successBackground' : '')
-          }
-          size="small"
-          onClick={() => onCopy('clientId', created.clientId)}
-        >
-          <Icon icon={copiedField === 'clientId' ? CheckIcon : CopyIcon} />
-        </IconButton>
-      </div>
+      <Input
+        name="client_id"
+        type="client_id"
+        title={t('createOAuthClient.client_id')}
+        value={created.clientId}
+        copyable={true}
+        readOnly
+      />
 
       {created.clientSecret && (
-        <div className="u-flex u-flex-items-center">
-          <div className="u-flex-grow-1">
-            <TextField
-              className="u-mt-1"
-              label={t('createOAuthClient.client_secret')}
-              value={created.clientSecret}
-              readOnly
-              fullWidth
-            />
-          </div>
-          <IconButton
-            className={
-              'u-ml-half u-mt-1' +
-              (copiedField === 'clientSecret' ? ' u-bg-successBackground' : '')
-            }
-            size="small"
-            onClick={() => onCopy('clientSecret', created.clientSecret)}
-          >
-            <Icon
-              icon={copiedField === 'clientSecret' ? CheckIcon : CopyIcon}
-            />
-          </IconButton>
-        </div>
+        <Input
+          name="client_secret"
+          type="client_secret"
+          title={t('createOAuthClient.client_secret')}
+          value={created.clientSecret}
+          copyable={true}
+          readOnly
+        />
       )}
     </>
   )
